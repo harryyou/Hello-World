7c53dbbc329c138e88e56208bae594d29968beed
harryyou <137248202@qq.com>
2016-10-26 10:42:51 +0800

    “aramiko 连接远程服务器模模块

---
+++ python_paramiko.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.5
+#coding:utf-8
+import paramiko
+host = '10.133.99.68'
+port = '22'
+user = 'ebss'
+mima = 'picc1234'
+paramiko.util.log_to_file('paramiko.log')
+server = paramiko.SSHClient()
+server.load_system_host_keys()
+#不加这段，可能会报'Server not found in known hosts'错误
+server.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#连接主机
+server.connect(host, port, user, mima)
+#执行命令
+stdin, stdout, stderr = server.exec_command('ls -l')
