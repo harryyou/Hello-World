b5da7a12b29455ec0e8d660893d6216e476d6c85
harryyou <137248202@qq.com>
2017-12-29 13:49:54 +0800

    删除注释，整理格式

--- django_practice/polls/views.py
+++ django_practice/polls/views.py
@@ -8,19 +8,10 @@ from .models import Question,Choice
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('pub_date')[:5]
-#    template = loader.get_template('polls/index.html')
     context = {'latest_question_list':latest_question_list,}
-    
-#    return HttpResponse(template.render(context,request))
     return render(request,'polls/index.html',context)
 
 def detail(request,question_id):
-    """
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404('question does not exist!')
-    """
     question = get_object_or_404(Question,pk=question_id)
     return render(request,'polls/detail.html',{'question':question})
 
@@ -34,7 +25,6 @@ def vote(request,question_id):
         selected_choice.votes +=1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results',args=(question.id,)))
-
 
 def results(request,question_id):
     question = get_object_or_404(Question,pk=question_id)
