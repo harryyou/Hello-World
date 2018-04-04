32b8830b7ed62d824a56cab5aea2d393c1c54697
harryyou <137248202@qq.com>
2018-04-04 15:41:37 +0800

    Show-me-the-code Practice0001:generate active code!

---
+++ Show-me-the-code/generate_active_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#coding:utf-8
+
+import string
+import random
+
+def GenerateActiveCode(n):
+    code_set = set()
+    while len(code_set) < n:
+        code = ''.join(random.sample(string.ascii_uppercase+string.digits,16))
+        code_set.add(code)
+    
+    return code_set
+    
+if __name__ == "__main__":
+print(GenerateActiveCode(200))
