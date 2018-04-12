8551bd732686b8a7ed0fce79ecadc552b65fe5cb
向不乐 <137248202@qq.com>
2018-04-12 16:06:15 +0800

    Show-me-the-code Practice0000:add text to the image!

---
+++ Show-me-the-code/image_draw_text.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+#coding:utf-8
+
+from PIL import Image,ImageDraw,ImageFont
+
+font = ImageFont.truetype("arial.ttf",36)
+color = (255,0,00)                  #rgb:red color
+
+def DrawText(img,text):
+    im = Image.open(img)            #Image Object
+    im_draw = ImageDraw.Draw(im)    #ImageDraw Object
+    
+    xy = (im.size[0] - 30,0)        #position of the text
+    
+    im_draw.text(xy,text,font=font,fill=color)
+    im.save(img)
+    
+if __name__ == '__main___':
+    img = 'qq.jpg'
+    text = "4"
+    DrawText(img,text)
+    
