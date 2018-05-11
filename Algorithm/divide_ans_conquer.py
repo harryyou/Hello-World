9dfadfe93b97f216f51ea6fe51c2152d3cdc8679
harryyou <137248202@qq.com>
2018-05-11 11:25:55 +0800

    use recursive to caculate the length of a list.

--- Algorithm/divide_ans_conquer.py
+++ Algorithm/divide_ans_conquer.py
@@ -2,12 +2,23 @@
 #coding:utf-8
 #D&C 分而治之算法
 
+#use recursvie algorithm to calculate the sum of a list.
 def sum(l):
     if l:
         return l.pop() + sum(l)
     else:
         return 0
 
+#use recursive to calculate the length of a list.
+def length(l):
+    if l:
+        l.pop()
+        return 1 + length(l)
+    else:
+        return 0
+
 if __name__ == "__main__":
     l = [i for i in range(101)]
     print(sum(l))
+    l1 = [ i for i in range(100)]
+    print(length(l1))
