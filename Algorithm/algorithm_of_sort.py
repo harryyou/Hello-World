65b2c11b73bac4e406bfe3cdfb7380bd1f1b38fc
137248202@qq.com <137248202@qq.com>
2018-07-17 14:54:50 +0800

    选择排序算法

--- Algorithm/algorithm_of_sort.py
+++ Algorithm/algorithm_of_sort.py
@@ -13,3 +13,27 @@ def quick_sort(l):
         larger = [ i for i in l[1:] if i > pivot]
 
         return quick_sort(smaller) + [pivot] + quick_sort(larger)
+
+
+#选择排序
+#selection sort
+#serarch the index of the smallest one
+def findsmallest(l):
+    smallest = l[0]
+    smallest_index= 0
+
+    for i in range(1,len(l)):
+        if l[i] < smallest:
+            smallest = l[i]
+            smallest_index = i
+    return smallest_index
+
+#sort
+def selection_sort(l):
+    new_l = []
+
+    for i in len(l):
+        smallest = findsmallest(l)
+        new_l.append(l.pop(smallest))
+
+    return new_l
